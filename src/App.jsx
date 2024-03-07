f9780b3e6a9a0b98da66626d28cc655e992af320
unknown <osamadrbas31@gmail.com>
2024-03-07 14:24:08 +0200

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import {Routes,Route,Link, BrowserRouter, useLocation} from 'react-router-dom'
 import { useRef, useState } from 'react'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_Storage_Bucket,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_Messaging_SenderId,
+  appId: import.meta.env.VITE_FIREBASE_AppId,
+  measurementId: import.meta.env.VITE_FIREBASE_MeasurementId
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 export default function App(){
 
 
 
 
     return(<>
-            App
+            App2
     </>)
 }
